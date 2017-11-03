7058652915fdaae09e8a3977ad1a3fae50a2dc90
Unknown <paulscotch1@gmail.com>
2017-11-03 09:37:12 -0500

    [Code] CSS Styles

--- src/addressapp/MainApp.java
+++ src/addressapp/MainApp.java
@@ -15,6 +15,7 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Modality;
@@ -57,6 +58,8 @@ public class MainApp extends Application {
     public void start(Stage primaryStage) {
         this.primaryStage = primaryStage;
         this.primaryStage.setTitle("AddressApp");
+        
+        this.primaryStage.getIcons().add(new Image("file:images/address_book_32.png"));
 
         initRootLayout();
 
